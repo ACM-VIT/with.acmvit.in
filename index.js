989f3d678be5939d3dcf2a99fa9994031250f127
Swarup Kharul <swarupkharulsk@gmail.com>
2021-07-11 19:29:08 +0530

    added: created a function to fetch data from firestore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("process.env.CREDENTIALS");
+var serviceAccount = require("./GOOGLE_APPLICATION_CREDENTIALS.json");
 
 const express = require('express');
 const app = express();
@@ -11,12 +11,15 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+let get_data = async () => {
+    const collectionQuerySnapshot = await db.collection("domainKeys").doc('cdRsoDCvIcIt4MpEkVbR').get();
+    return collectionQuerySnapshot.data();
+}
 
 // redirect :id to specific webpage
 app.get('/:id', async function (req, res) {
     const param = req.params.id;
-    const collectionQuerySnapshot = await db.collection("domainKeys").doc('cdRsoDCvIcIt4MpEkVbR').get();
-    const maps = collectionQuerySnapshot.data();
+    const maps = get_data()
     // const redirect_to = maps.map(({ title, url }) => { if (title == param) return url; });
     res.redirect(301, maps[param]);
 });
