e46f65b6cff939eb5a06b9f4191bfba31665d638
Swarup Kharul <swarupkharulsk@gmail.com>
2021-07-11 19:18:09 +0530

    integration: integrated firestore into the application

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("process.env.CREDENTIALS");
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
-const maps = require('./map');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
 
 // redirect :id to specific webpage
-app.get('/:id', function (req, res) {
+app.get('/:id', async function (req, res) {
     const param = req.params.id;
-    const redirect_to = maps.map(({ title, url }) => { if (title == param) return url; });
-    res.redirect(301, redirect_to[0]);
+    const collectionQuerySnapshot = await db.collection("domainKeys").doc('cdRsoDCvIcIt4MpEkVbR').get();
+    const maps = collectionQuerySnapshot.data();
+    // const redirect_to = maps.map(({ title, url }) => { if (title == param) return url; });
+    res.redirect(301, maps[param]);
 });
 
 app.listen(port);
