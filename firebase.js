03cd711603a8a8d15d48c6cc4317c7d26f1f6ba3
cr-trojan23 <pandusrinu1604@gmail.com>
2021-07-13 01:16:18 +0530

    corrected typo in firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const serviceAccountObject = {
   project_id: "acm-keyholder",
   private_key_id: process.env.PRIVATE_KEY_ID,
   private_key: process.env.PRIVATE_KEY,
-  client_email: process.env.CLIENT_EMAILL,
+  client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
