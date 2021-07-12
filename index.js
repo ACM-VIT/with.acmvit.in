cf9e67613861b28ca04a8fe3900b17147610347d
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-07-12 22:16:26 +0530

    feat(app): integrate firebase

--- index.js
+++ index.js
@@ -1,52 +1,74 @@
 require("dotenv").config();
 
-const config = require("config");
-const admin = require("firebase-admin");
 const express = require("express");
-
 const app = express();
 const port = 3000;
 
-const serviceAccountObject = {
-  type: "service_account",
-  project_id: "acm-keyholder",
-  private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
-  client_email: process.env.CLIENT_EMAILL,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: process.env.CERT_URL,
-};
+/** load peer services */
+const { worker } = require("./firebase");
+const {
+  getMemory,
+  setMemory,
+  setMemoryArray,
+  triggerLastUpdated,
+  isMemoryEmpty,
+  clearMemory,
+} = require("./memory");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountObject),
+/** route to return success on root */
+app.get("/", (req, res) => {
+  res.json({ success: true });
 });
 
-const db = admin.firestore();
-
-let routeMaps = [];
-
-const get_data = async () => {
-  const collectionQuerySnapshot = await db
-    .collection("domainKeys")
-    .doc("cdRsoDCvIcIt4MpEkVbR")
-    .get();
-  routeMaps = await collectionQuerySnapshot.data();
-  return routeMaps;
-};
-
-/** run the get_data route to fetch more results */
+/** to handle database updates and memory saves */
 app.get("/update", async (req, res) => {
-  const data = await get_data();
-  return res.json(data);
+  try {
+    const routeMappings = await worker();
+    clearMemory();
+    setMemoryArray(routeMappings);
+    return res.json({ success: true });
+  } catch (e) {
+    return res.json({ success: false, error: e.message });
+  }
 });
 
-// // redirect :id to specific webpage
-// app.get("/:id", async function (req, res) {
-//   const param = req.params.id;
-//   res.redirect(301, routeMaps[param]);
-// });
+/** to list all routes */
+app.get("/debug", (req, res) => {
+  return res.json(getMemory());
+});
 
-app.listen(port);
+/** to accept an id in url param and call the service */
+app.get("/:id", async (req, res) => {
+  /** if memory is empty, then call database */
+
+  if (isMemoryEmpty()) {
+    try {
+      const routeMappings = await worker();
+      setMemoryArray(routeMappings);
+      console.log(getMemory());
+    } catch (e) {
+      return res.json({ success: false, error: e.message });
+    }
+  }
+
+  /** get id from URL param */
+  const { id } = req.params;
+
+  /** loop over all routes stored in memory and see if key matches */
+  const routes = getMemory();
+
+  try {
+    if (routes[id]) {
+      return res.status(301).redirect(routes[id]);
+    }
+  } catch (e) {
+    return res.json({ success: false, error: e.message });
+  }
+
+  return res.json({ success: "looks like you're lost :(" });
+});
+
+/** listen for connections */
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
