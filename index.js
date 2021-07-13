610d4fd257f1d4a7901bd7624a257de21aa637d0
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-07-13 12:57:12 +0530

    chore(app): use native redirect

--- index.js
+++ index.js
@@ -1,74 +1,78 @@
 require("dotenv").config();
 
-
 const express = require("express");
 const maps = require("./map");
 const app = express();
 const port = 3000;
 
-
-/** load peer services */
-const { worker } = require("./firebase");
-const {
-  getMemory,
-  setMemory,
-  setMemoryArray,
-  triggerLastUpdated,
-  isMemoryEmpty,
-  clearMemory,
-} = require("./memory");
+// /** load peer services */
+// const { worker } = require("./firebase");
+// const {
+//   getMemory,
+//   setMemory,
+//   setMemoryArray,
+//   triggerLastUpdated,
+//   isMemoryEmpty,
+//   clearMemory,
+// } = require("./memory");
 
 /** route to return success on root */
 app.get("/", (req, res) => {
-  res.json({ success: true });
+  res.status(301).redirect("https://bootcamp.acmvit.in");
+  return res.json({ success: true });
 });
 
 /** to handle database updates and memory saves */
-app.get("/update", async (req, res) => {
-  try {
-    const routeMappings = await worker();
-    clearMemory();
-    setMemoryArray(routeMappings);
-    return res.json({ success: true });
-  } catch (e) {
-    return res.json({ success: false, error: e.message });
-  }
-});
+// app.get("/update", async (req, res) => {
+//   try {
+//     const routeMappings = await worker();
+//     clearMemory();
+//     setMemoryArray(routeMappings);
+//     return res.json({ success: true });
+//   } catch (e) {
+//     return res.json({ success: false, error: e.message });
+//   }
+// });
 
 /** to list all routes */
-app.get("/debug", (req, res) => {
-  return res.json(getMemory());
-});
+// app.get("/debug", (req, res) => {
+//   return res.json(getMemory());
+// });
 
 /** to accept an id in url param and call the service */
 app.get("/:id", async (req, res) => {
   /** if memory is empty, then call database */
 
-  if (isMemoryEmpty()) {
-    try {
-      const routeMappings = await worker();
-      setMemoryArray(routeMappings);
-      console.log(getMemory());
-    } catch (e) {
-      return res.json({ success: false, error: e.message });
-    }
-  }
+  // if (isMemoryEmpty()) {
+  //   try {
+  //     const routeMappings = await worker();
+  //     setMemoryArray(routeMappings);
+  //     console.log(getMemory());
+  //   } catch (e) {
+  //     return res.json({ success: false, error: e.message });
+  //   }
+  // }
 
   /** get id from URL param */
-  const { id } = req.params;
+  const { id: param } = req.params;
 
-  /** loop over all routes stored in memory and see if key matches */
-  const routes = getMemory();
+  // /** loop over all routes stored in memory and see if key matches */
+  // const routes = getMemory();
 
-  try {
-    if (routes[id]) {
-      return res.status(301).redirect(routes[id]);
-    }
-  } catch (e) {
-    return res.json({ success: false, error: e.message });
-  }
+  // try {
+  //   if (routes[id]) {
+  //     return res.status(301).redirect(routes[id]);
+  //   }
+  // } catch (e) {
+  //   return res.json({ success: false, error: e.message });
+  // }
+  const redirect_to = maps.map(({ title, url }) => {
+    if (title == param) return url;
+  });
 
-  return res.json({ success: "looks like you're lost :(" });
+  console.log(redirect_to);
+  res.status(301).redirect(redirect_to);
+  // return res.json({ success: "looks like you're lost :(" });
 });
 
 /** listen for connections */
--- map.js
+++ map.js
@@ -3,10 +3,10 @@ const maps = [
     title: "web-bootcamp",
     url: "https://youtu.be/s7UR06BW5LY",
   },
-	{
-		title: "bootcamp-web-resources",
-		url: "https://www.notion.so/Building-a-portfolio-in-React-js-e94ebf98b1ed482abd773431a06be15a"
-	}
+  {
+    title: "bootcamp-web-resources",
+    url: "https://www.notion.so/Building-a-portfolio-in-React-js-e94ebf98b1ed482abd773431a06be15a",
+  },
 ];
 
 module.exports = maps;
